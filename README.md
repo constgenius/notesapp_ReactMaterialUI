ed7f6e7549838d9a33a50bdf1a20176559e42afb
robin8778 <robin.007660@gmail.com>
2023-09-06 13:46:07 +0530

    Tutorial2

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Welcome to the React Material UI Tutorial - Intro & Setup! This tutorial will help you set up a basic React project with Material-UI, a popular UI library for React applications. Material-UI provides a set of pre-designed, customizable components that follow Google's Material Design guidelines, making it easier to create modern and visually appealing user interfaces.
 
-![Tutorial 1](public/MaterialUI1.png)
+![Tutorial 1](public/MaterialUI2.png)
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,8 +1,31 @@
 import React from "react"
+import { Typography } from "@mui/material"
+
 const Create = () => {
   return (
     <div>
-      Create
+      <Typography
+        variant="h6" 
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      {/* <Typography
+        variant="h1" 
+        color="primary"
+        align="center"
+      >
+        Create a New Note
+      </Typography>
+      <Typography
+        noWrap
+        color="secondary"
+      >
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque molestiae magnam voluptate reiciendis fuga totam facere officia, animi sint laboriosam dolorum voluptas cupiditate explicabo dolore veritatis recusandae rem inventore sunt.
+      </Typography> */}
     </div>
   )
 }
