be61e650715eca4f00bfb57c954ebc50500e75fe
robin8778 <robin.007660@gmail.com>
2023-09-07 11:19:59 +0530

    Tutorial-9

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,16 +1,19 @@
-import React,{useState,useEffect} from "react"
+import React, { useEffect, useState } from "react"
 const Notes = () => {
-  const [notes, setNotes] = useState([]);
+  const [notes,setNotes]=useState([]);
 
-  useEffect(() => {
-    fetch('http://localhost:8000/notes')
-      .then(res => res.json())
-      .then(data => setNotes(data))
-  }, [])
+  useEffect(()=>{
+    fetch("http://localhost:8000/notes")
+    .then(res => res.json())
+    .then(data => setNotes(data))
+  },[])
+
   return (
-    <div>{notes.map(note => (
-      <p key={note.id}>{ note.title }</p>
-    ))}</div>
+    <div>
+      {notes.map(note=>(
+        <p key={note.id}>{note.title}</p>
+      ))}
+    </div>
     
   )
 }
