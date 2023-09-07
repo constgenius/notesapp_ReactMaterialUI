b0ee9c1c84042a79a57418de832706d6a45f5e3c
robin8778 <robin.007660@gmail.com>
2023-09-07 08:08:26 +0530

    Final Changes

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,35 +1,83 @@
-import React from "react"
+import React, { useState } from "react"
 import { Typography } from "@mui/material";
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { makeStyles } from "@mui/styles";
+import TextField from '@mui/material/TextField';
 
-const useStyles = makeStyles({
-})
+
 
 const Create = () => {
-  const classes = useStyles();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    setTitleError(false)
+    setDetailsError(false)
+
+    if(title==""){
+      setTitleError(true)
+    }
+
+    if(details==""){
+      setDetailsError(true);
+    }
+
+    if(title && details){
+      console.log(title,details);
+    }
+  }
   return (
     <Container>
       <Typography
-        variant="h6" 
+        variant="h6"
         color="textSecondary"
         component="h2"
         gutterBottom
       >
         Create a New Note
       </Typography>
-      <Button
-        onClick={()=>console.log("You Clicked Me")}
-        type="submit"
-        color="secondary"
-        variant="contained"
-        endIcon={<KeyboardArrowRightIcon/>}
-      >
-        Submit
-      </Button>
-      
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          sx={{ mt: 2, mb: 2 }}
+          label="Note Title"
+          color="secondary"
+          variant="outlined"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          sx={{ mt: 2, mb: 2 }}
+          label="Details"
+          color="secondary"
+          variant="outlined"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
+
+        <Button
+          type="submit"
+          color="secondary"
+          variant="contained"
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+
+      </form>
+
+
+
 
     </Container>
   )
