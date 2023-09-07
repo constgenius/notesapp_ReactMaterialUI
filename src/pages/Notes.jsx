33358a4d30197dc018b429eeedc4ca091bc1c315
robin8778 <robin.007660@gmail.com>
2023-09-07 13:33:02 +0530

    Tutorial-10

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,20 +1,45 @@
 import React, { useEffect, useState } from "react"
-const Notes = () => {
-  const [notes,setNotes]=useState([]);
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Container from '@mui/material/Container';
 
-  useEffect(()=>{
+const Notes = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
     fetch("http://localhost:8000/notes")
-    .then(res => res.json())
-    .then(data => setNotes(data))
-  },[])
+      .then(res => res.json())
+      .then(data => setNotes(data))
+  }, [])
 
   return (
-    <div>
-      {notes.map(note=>(
-        <p key={note.id}>{note.title}</p>
-      ))}
-    </div>
-    
+    <Container>
+      {/* <Grid container spacing={3}>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>1</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>2</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>3</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>4</Paper>
+        </Grid>
+      </Grid> */}
+
+      <Grid container spacing={3}>
+        {notes.map(note => (
+          <Grid item xs={12} md={6} lg={4} key={note.id}>
+            <Paper>
+              {note.title}
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+
   )
 }
 
