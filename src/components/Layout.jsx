1534b03a09c0d36e5ca93f8a70be8d956d896c07
robin8778 <robin.007660@gmail.com>
2023-09-09 07:12:37 +0530

    Tutorial-14

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import Drawer from '@mui/material/Drawer';
 import { Typography } from '@mui/material';
-import Box from '@mui/material';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { SubjectOutlined,AddCircleOutlineOutlined } from '@mui/icons-material';
 
 const drawerWidth=240;
+const menuItems = [
+  { 
+    text: 'My Notes', 
+    icon: <SubjectOutlined color="secondary" />, 
+    path: '/' 
+  },
+  { 
+    text: 'Create Note', 
+    icon: <AddCircleOutlineOutlined color="secondary" />, 
+    path: '/create' 
+  },
+];
+
 
 const Layout = ({children}) => {
   return (
-    <Box sx={{display:"flex"}}>
-        {/* app bar */}
+    <Box sx={{ display: "flex" }}>
+      {/* app bar */}
 
-        {/* Side Bar */}
-        <Drawer
+      {/* Side Bar */}
+      <Drawer
         sx={{
           width: drawerWidth,
           flexShrink: 0,
@@ -20,19 +39,37 @@ const Layout = ({children}) => {
             boxSizing: 'border-box',
           },
         }}
-        variant="persistent"
+        variant="permanent"
         anchor="left"
-        open={open}
       >
         <Typography variant='h5'>Genius Notes</Typography>
+
+
+        {/* Lists and List Items */}
+        <List>
+                {menuItems.map((item) => (
+                  <ListItem
+                    key={item.text}
+                    disablePadding
+                  >
+                  <ListItemButton>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.text} />
+                  </ListItemButton>
+                    
+                  </ListItem>
+                ))}
+              </List>
+
+
       </Drawer>
 
-        <Box sx={{width:"100%",backgroundColor:"#f9f9f9"}}>
-            {children}
-        </Box>
-    
+      <Box sx={{ width: "100%", backgroundColor: "#f9f9f9" }}>
+        {children}
+      </Box>
+
     </Box>
-  )
+  );
 }
 
 export default Layout
