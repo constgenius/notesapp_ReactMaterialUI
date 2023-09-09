5d194874b6b2b07823664579f6db1bd053c38bc7
robin8778 <robin.007660@gmail.com>
2023-09-09 13:27:20 +0530

    Tutorial-18

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -3,6 +3,8 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
 import NoteCard from "../components/NoteCard";
+import Masonry from 'react-masonry-css'
+import { Box } from "@mui/material";
 
 const Notes = () => {
   const [notes, setNotes] = useState([]);
@@ -13,24 +15,32 @@ const Notes = () => {
       .then(data => setNotes(data))
   }, [])
 
-  const handleDelete = async(id)=>{
-    await fetch("http://localhost:8000/notes/"+id,{
-      method:"DELETE"
+  const handleDelete = async (id) => {
+    await fetch("http://localhost:8000/notes/" + id, {
+      method: "DELETE"
     })
-    const newNotes = notes.filter(note => note.id !=id);
+    const newNotes = notes.filter(note => note.id != id);
     setNotes(newNotes);
   }
 
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1,
+  };
   return (
     <Container>
-      
-      <Grid container spacing={3}>
+
+      <Masonry
+        breakpointCols={breakpoints}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column">
         {notes.map(note => (
-          <Grid item xs={12} md={6} lg={4} key={note.id}>
-            <NoteCard note={note} handleDelete={handleDelete}/>
-          </Grid>
+          <Box key={note.id}>
+            <NoteCard note={note} handleDelete={handleDelete} />
+          </Box>
         ))}
-      </Grid>
+      </Masonry>
     </Container>
 
   )
